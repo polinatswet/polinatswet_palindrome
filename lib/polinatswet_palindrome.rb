4dd542edca18a3a1e2dcbac294fd1b608398fe71
Полина Цветкова <polinacvetkova@MacBook-Air-Polina.local>
2024-10-15 19:16:51 +0700

    initialize gem skeletone

---
+++ lib/polinatswet_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "polinatswet_palindrome/version"
+
+module PolinatswetPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/polinatswet_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PolinatswetPalindrome
+  VERSION = "0.1.0"
+end
