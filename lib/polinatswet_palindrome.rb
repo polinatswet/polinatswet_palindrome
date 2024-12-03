ca3ebeb54d28fb58a0a2c06b0bca3dc370f9fc7a
Полина Цветкова <polinacvetkova@MacBook-Air-Polina.local>
2024-12-03 22:02:24 +0700

    testing-chapter-8.5.-refactor

--- lib/polinatswet_palindrome.rb
+++ lib/polinatswet_palindrome.rb
@@ -1,35 +1,17 @@
-# frozen_string_literal: true
 # putting the palindrome detector in the gem
 
 require 'polinatswet_palindrome/version'
 
-#module PolinatswetPalindrome
 class String
-#  returns true for a palindrome ,false otherwise.
+  # returns true for a palindrome ,false otherwise.
   def palindrome?
     processed_content == processed_content.reverse
   end
 
-  # возвращает буквы в строке
-  def letters
-    # создать массив символов, которые соответствуют заглавным или строчим буквам
-    the_letters = []
-    # итерация
-    for i in 0..self.length-1
-      if self[i].match(/[a-zA-Z]/)
-        # pushing each character onto the array, if it matches the letter regex
-        the_letters << self[i]
-      end
-    end
-    # Сформировать строку букв в исходной строке
-    the_letters.join
-  end
-
   private
 
-  # return content for palindrome testing
+  # returns content for palindrome testing
   def processed_content
-    self.letters.downcase
+   scan(/[a-z]/i).join.downcase
   end
 end
-#end
